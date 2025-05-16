8a38dac7fb4ef1c42005342ec0ae7c8dfa24c908
sehajmakkar <codewithsehaj@gmail.com>
2025-05-17 03:16:22 +0530

    hero section - learnt a lot - parallax still left

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,7 +5,7 @@ import MenuIcon from "@/assets/menu.svg";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
+    <header className="sticky top-0 backdrop-blur-sm z-20">
       {/* banner */}
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <div className="inline-flex gap-1 items-center">
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,60 @@
+import ArrowRight from "@/assets/arrow-right.svg";
+import Image from "next/image";
+import cogImage from "@/assets/cog.png";
+import cylinderImage from "@/assets/cylinder.png";
+import noodleImage from "@/assets/noodle.png";
+
 export const Hero = () => {
-  return null;
+  return (
+    // gradient from #183EC2 to #EAEEFE
+    // background: radial-gradient(ellipse 200% 100% at bottom left, #183EC2, #EAEEFE 66%);
+    // aise add karte hai background mein gradient BC!
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="md:flex items-center">
+          <div className="md:w-[478px]">
+            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+              Version 2.0 is here
+            </div>
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+              Pathway to Productivity
+            </h1>
+            <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+              Unlock the full potential of your workflow with our latest update.
+              Experience enhanced features and improved performance for a
+              seamless experience.
+            </p>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free!</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowRight className="h-5 w-5 inline-flex justify-center align-center" />
+              </button>
+            </div>
+          </div>
+
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+            <Image
+              src={cogImage}
+              alt="cog"
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+            />
+            <Image
+              src={cylinderImage}
+              width={220}
+              height={220}
+              alt="cylinder"
+              className="hidden md:block -top-8 -left-32 md:absolute"
+              />
+            <Image
+              src={noodleImage}
+              width={220}
+              alt="noodle"
+              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
