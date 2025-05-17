59809cdf5e1999d1d646794be1ffca88e6f01af4
sehajmakkar <codewithsehaj@gmail.com>
2025-05-18 01:36:21 +0530

    project is done!!

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,11 +1,20 @@
+"use client";
 import ArrowRightIcon from "@/assets/arrow-right.svg";
 import starImage from "@/assets/star.png";
 import springImage from "@/assets/spring.png";
-import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const CallToAction = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150,-150]);
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+    <section ref={ref} className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="section-heading relative">
           <h2 className="section-title">Sign up for free today!</h2>
@@ -14,15 +23,17 @@ export const CallToAction = () => {
             applications with our platform. Sign up now and get access to
             exclusive resources, tutorials, and support from our team.
           </p>
-          <Image 
-          src={starImage} 
+          <motion.img 
+          src={starImage.src} 
           alt="Star" 
           width={360}
+          style={{ translateY }}
           className="absolute -left-[350px] -top-[137px]" />
-          <Image 
-          src={springImage} 
+          <motion.img 
+          src={springImage.src} 
           alt="Spring" 
           width={360}
+          style={{ translateY }}
           className="absolute -right-[331px] -top-[19px]" />
         </div>
         <div className="flex gap-2 mt-10 justify-center">
