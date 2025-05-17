73e7a5ecd903bee8c00c0986c5b965bc959c464e
sehajmakkar <codewithsehaj@gmail.com>
2025-05-17 19:14:22 +0530

    product showcase - kya sundar lag raha  hai.

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,44 @@
+import Image from "next/image";
+import productImage from "@/assets/product-image.png";
+import pyramidImage from "@/assets/pyramid.png";
+import tubeImage from "@/assets/tube.png";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Boost Your Productivity</div>
+          </div>
+          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+            Effortlessly track your progress and stay on top of your tasks with
+            our intuitive dashboard. Our platform provides you with the tools
+            you need to manage your projects efficiently, so you can focus on
+            what really matters.
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={productImage} alt="Product Image" className="mt-10" />
+          <Image
+            src={pyramidImage}
+            alt="Pyramid Image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+          />
+          <Image
+            src={tubeImage}
+            alt="Tube Image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -left-36 -bottom-24"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
