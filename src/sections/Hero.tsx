f59cbe62ab21dd3717ef4e235279e52ed8c40b47
sehajmakkar <codewithsehaj@gmail.com>
2025-05-18 00:40:48 +0530

    hero section animation + parallax effect done!

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,15 +1,27 @@
+"use client";
 import ArrowRight from "@/assets/arrow-right.svg";
-import Image from "next/image";
 import cogImage from "@/assets/cog.png";
 import cylinderImage from "@/assets/cylinder.png";
 import noodleImage from "@/assets/noodle.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const Hero = () => {
+  // parallax effect on the cylinder and noodle images
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+
   return (
     // gradient from #183EC2 to #EAEEFE
     // background: radial-gradient(ellipse 200% 100% at bottom left, #183EC2, #EAEEFE 66%);
     // aise add karte hai background mein gradient BC!
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section ref={heroRef} className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
@@ -34,23 +46,40 @@ export const Hero = () => {
           </div>
 
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image
-              src={cogImage}
+            <motion.img
+              src={cogImage.src}
               alt="cog"
               className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+              // animating the cog image to move up and down 
+              animate={{
+                translateY: [-30, 30],
+              }}
+              transition={{
+                duration: 3,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatType: "mirror",
+              }}
             />
-            <Image
-              src={cylinderImage}
+            <motion.img
+              src={cylinderImage.src}
               width={220}
               height={220}
               alt="cylinder"
               className="hidden md:block -top-8 -left-32 md:absolute"
+              style={{
+                translateY: translateY,
+              }}
               />
-            <Image
-              src={noodleImage}
+            <motion.img
+              src={noodleImage.src}
               width={220}
               alt="noodle"
-              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+              className="hidden lg:block absolute top-[524px] left-[448px]"
+              style={{
+                rotate: 30,
+                translateY: translateY,
+              }}
             />
           </div>
         </div>
